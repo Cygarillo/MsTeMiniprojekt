323203332465674ee2406f5c4fec3ae51a4daa1c
fsenn <fsenn@hsr.ch>
2012-11-21 11:35:59 +0100

    DTOs

--- AutoReservation.Common/DataTransferObjects/AutoDto.cs
+++ AutoReservation.Common/DataTransferObjects/AutoDto.cs
@@ -7,49 +7,58 @@ namespace AutoReservation.Common.DataTransferObjects
     public class AutoDto //: DtoBase
     {
 
-        //public override string Validate()
-        //{
-        //    StringBuilder error = new StringBuilder();
-        //    if (string.IsNullOrEmpty(marke))
-        //    {
-        //        error.AppendLine("- Marke ist nicht gesetzt.");
-        //    }
-        //    if (tagestarif <= 0)
-        //    {
-        //        error.AppendLine("- Tagestarif muss grösser als 0 sein.");
-        //    }
-        //    if (AutoKlasse == AutoKlasse.Luxusklasse && basistarif <= 0)
-        //    {
-        //        error.AppendLine("- Basistarif eines Luxusautos muss grösser als 0 sein.");
-        //    }
+        public override string Validate()
+        {
+            StringBuilder error = new StringBuilder();
+            if (string.IsNullOrEmpty(marke))
+            {
+                error.AppendLine("- Marke ist nicht gesetzt.");
+            }
+            if (tagestarif <= 0)
+            {
+                error.AppendLine("- Tagestarif muss grösser als 0 sein.");
+            }
+            if (AutoKlasse == AutoKlasse.Luxusklasse && basistarif <= 0)
+            {
+                error.AppendLine("- Basistarif eines Luxusautos muss grösser als 0 sein.");
+            }
 
-        //    if (error.Length == 0) { return null; }
+            if (error.Length == 0) { return null; }
 
-        //    return error.ToString();
-        //}
+            return error.ToString();
+        }
 
-        //public override object Clone()
-        //{
-        //    return new AutoDto
-        //    {
-        //        Id = Id,
-        //        Marke = Marke,
-        //        Tagestarif = Tagestarif,
-        //        AutoKlasse = AutoKlasse,
-        //        Basistarif = Basistarif
-        //    };
-        //}
-        
-        //public override string ToString()
-        //{
-        //    return string.Format(
-        //        "{0}; {1}; {2}; {3}; {4}",
-        //        Id,
-        //        Marke,
-        //        Tagestarif,
-        //        Basistarif,
-        //        AutoKlasse);
-        //}
+        public override object Clone()
+        {
+            return new AutoDto
+            {
+                Id = Id,
+                Marke = Marke,
+                Tagestarif = Tagestarif,
+                AutoKlasse = AutoKlasse,
+                Basistarif = Basistarif
+            };
+        }
 
+        public override string ToString()
+        {
+            return string.Format(
+                "{0}; {1}; {2}; {3}; {4}",
+                Id,
+                Marke,
+                Tagestarif,
+                Basistarif,
+                AutoKlasse);
+        }
+        [DataMember]
+        public int Basistarif { get; set; }
+        [DataMember]
+        public int Tagestarif { get; set; }
+        [DataMember]
+        public int Id { get; set; }
+        [DataMember]
+        public string Marke { get; set; }
+        [DataMember]
+        public AutoKlasse AutoKlasse { get; set; }
     }
 }
--- AutoReservation.Common/DataTransferObjects/KundeDto.cs
+++ AutoReservation.Common/DataTransferObjects/KundeDto.cs
@@ -5,50 +5,58 @@ using System.Text;
 namespace AutoReservation.Common.DataTransferObjects
 {
     [DataContract]
-    public class KundeDto //: DtoBase
+    public class KundeDto : DtoBase
     {
 
-        //public override string Validate()
-        //{
-        //    StringBuilder error = new StringBuilder();
-        //    if (string.IsNullOrEmpty(Nachname))
-        //    {
-        //        error.AppendLine("- Nachname ist nicht gesetzt.");
-        //    }
-        //    if (string.IsNullOrEmpty(Vorname))
-        //    {
-        //        error.AppendLine("- Vorname ist nicht gesetzt.");
-        //    }
-        //    if (Geburtsdatum == DateTime.MinValue)
-        //    {
-        //        error.AppendLine("- Geburtsdatum ist nicht gesetzt.");
-        //    }
+        public override string Validate()
+        {
+            StringBuilder error = new StringBuilder();
+            if (string.IsNullOrEmpty(Nachname))
+            {
+                error.AppendLine("- Nachname ist nicht gesetzt.");
+            }
+            if (string.IsNullOrEmpty(Vorname))
+            {
+                error.AppendLine("- Vorname ist nicht gesetzt.");
+            }
+            if (Geburtsdatum == DateTime.MinValue)
+            {
+                error.AppendLine("- Geburtsdatum ist nicht gesetzt.");
+            }
 
-        //    if (error.Length == 0) { return null; }
+            if (error.Length == 0) { return null; }
 
-        //    return error.ToString();
-        //}
+            return error.ToString();
+        }
 
-        //public override object Clone()
-        //{
-        //    return new KundeDto
-        //    {
-        //        Id = Id,
-        //        Nachname = Nachname,
-        //        Vorname = Vorname,
-        //        Geburtsdatum = Geburtsdatum
-        //    };
-        //}
+        public override object Clone()
+        {
+            return new KundeDto
+            {
+                Id = Id,
+                Nachname = Nachname,
+                Vorname = Vorname,
+                Geburtsdatum = Geburtsdatum
+            };
+        }
 
-        //public override string ToString()
-        //{
-        //    return string.Format(
-        //        "{0}; {1}; {2}; {3}",
-        //        Id,
-        //        Nachname,
-        //        Vorname,
-        //        Geburtsdatum);
-        //}
+        public override string ToString()
+        {
+            return string.Format(
+                "{0}; {1}; {2}; {3}",
+                Id,
+                Nachname,
+                Vorname,
+                Geburtsdatum);
+        }
 
+        [DataMember]
+        public int Id { get; set; }
+        [DataMember]
+        public string Nachname { get; set; }
+        [DataMember]
+        public string Vorname { get; set; }
+        [DataMember]
+        public DateTime Geburtsdatum { get; set; }
     }
 }
