9478c04cd355f5b302a07a3140a4e78347369553
Cyrill Lam <cyrill.lam@gmail.com>
2012-11-22 13:21:09 +0100

    added missing classes

---
+++ AutoReservation.Ui/Factory/Creator.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using AutoReservation.Common.Interfaces;
+using AutoReservation.Ui.Properties;
+
+namespace AutoReservation.Ui.Factory
+{
+    abstract class Creator
+    {
+        public abstract IAutoReservationService CreateInstance();
+        public Creator GetCreator()
+        {
+            Type serviceLayerType = Type.GetType(Settings.Default.ServiceLayerType);
+            if (serviceLayerType == null) { return new LocalDataAccessCreator(); }
+            return (Creator)Activator.CreateInstance(serviceLayerType);   
+        }
+    }
+
+    internal class LocalDataAccessCreator : Creator
+    {
+        public override IAutoReservationService CreateInstance()
+        {
+            
+        }
+    }
+}
